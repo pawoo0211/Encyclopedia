e668365054a22f9e7460cfa31a32cc5611dd959f
pawoo0211 <pawoo0211@naver.com>
2022-06-01 15:22:42 +0900

    refactor: 단어 생성 API
    단어가 이미 존재하는 경우 해당 단어는 생성되지 못하도록 처리

--- src/main/java/com/example/encyclopedia/subject/domain/WordRepository.java
+++ src/main/java/com/example/encyclopedia/subject/domain/WordRepository.java
@@ -3,5 +3,6 @@ package com.example.encyclopedia.subject.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WordRepository extends JpaRepository<Word,Long> {
-
+    boolean existsByNameAndSubject(String WordName, Subject subject);
+    /* 이미 생성된 단어가 있을 경우 해당 단어와 동일한 단어를 생성하지 않도록 하기 위한 Method */
 }
--- src/main/java/com/example/encyclopedia/subject/service/WordService.java
+++ src/main/java/com/example/encyclopedia/subject/service/WordService.java
@@ -17,8 +17,12 @@ public class WordService {
     private final WordRepository wordRepository;
 
     public ResponseDto createWord(CreateWordRequestDto createWordRequestDto) {
-        if(subjectRepository.existsByName(createWordRequestDto.getSubjectName())){
+        if (subjectRepository.existsByName(createWordRequestDto.getSubjectName())) {
             Subject subject = subjectRepository.findByName(createWordRequestDto.getSubjectName());
+
+            if(wordRepository.existsByNameAndSubject(createWordRequestDto.getWordName(),subject)){
+                return new ResponseDto("해당 단어는 이미 존재하는 단어입니다.",createWordRequestDto.getWordName());
+            }
 
             Word word = new Word();
 
@@ -30,18 +34,20 @@ public class WordService {
 
             return new ResponseDto("단어 생성", word.getName());
         }
+        else {
+            Subject subject = new Subject();
+            Word word = new Word();
 
-        Subject subject = new Subject();
-        Word word = new Word();
+            subject.setName(createWordRequestDto.getSubjectName());
+            word.setName(createWordRequestDto.getWordName());
+            word.setMeaning(createWordRequestDto.getMeaning());
+            word.setSubject(subject);
 
-        subject.setName(createWordRequestDto.getSubjectName());
-        word.setName(createWordRequestDto.getWordName());
-        word.setMeaning(createWordRequestDto.getMeaning());
-        word.setSubject(subject);
+            subjectRepository.save(subject);
+            wordRepository.save(word);
 
-        subjectRepository.save(subject);
-        wordRepository.save(word);
+            return new ResponseDto("단어 생성", word.getName());
 
-        return new ResponseDto("단어 생성", word.getName());
+        }
     }
 }
